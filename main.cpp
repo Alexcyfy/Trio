cdfa5d2055cdc6a1cbc1d19dd5dbbc93cb5d5078
Alexcyfy <acollanton@mail.csuchico.edu>
2018-11-07 16:47:22 -0800

    Arranges from biggest to smallest

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
+void swap(int &, int &, int &);
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 
 
   // <-- ADD YOUR FUNCTION CALL HERE
-
+swap(red, green, blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -23,3 +24,56 @@ int main()
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+void swap(int &red, int &green, int &blue)
+{
+/*vector <int> colorValues;
+colorValues.push_back(red);
+colorValues.push_back(green);
+colorValues.push_back(blue);*/
+int tempred = red;
+int tempblue = blue;
+int tempgreen = green;
+if(red > green && green > blue)
+{
+red = tempred;
+green = tempgreen;
+blue = tempblue;
+}
+if(red > blue && blue > green)
+{
+red = tempred;
+green = tempblue;
+blue = tempgreen;
+}
+if(green > red && red > blue)
+{
+red = tempgreen;
+green = tempred;
+blue = tempblue;
+}
+if(green > blue && blue > red)
+{
+red = tempgreen;
+green = tempblue;
+blue = tempred;
+}
+if(blue > red && red > green)
+{
+red = tempblue;
+green = tempred;
+blue = tempgreen;
+}
+if(blue > green && green > red)
+{
+red = tempblue;
+green = tempgreen;
+blue = tempred;
+}
+
+ 
+  
+}
+
+
+
+
